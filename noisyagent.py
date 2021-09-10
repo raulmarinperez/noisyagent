c2df7a9460cc802e6d037ddcc5f45665e8daa2e2
Raul Marin <raulmarinperez@gmail.com>
2021-09-10 08:52:37 -0700

    First complete implementation release

--- noisyagent.py
+++ noisyagent.py
@@ -1,10 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from event_scheduler import EventScheduler
+from datetime import datetime, timedelta
 from threading import Thread
+from pathlib import Path
+import requests
 import argparse
+import requests
 import logging
 import yaml
+import json
 import time
+import glob
 import os
 
 release="0.1"
@@ -15,6 +21,28 @@ release="0.1"
 class NewsFeedRequestHandler(BaseHTTPRequestHandler):
   ''' Auxiliary class implementing a HTTPRequestHandler for our web server.
   '''
+
+  def update_dates(self, news):
+    ''' Function replacing all date place holders in the input parameter
+        Parameters:
+          news - String with the news that the web server will server
+                 containing place holders to be replaced.
+        Return:
+          A string containing the news and the placeholder containing actual
+          dates.
+    '''
+
+    # 1. Use current date as reference data and replace
+    #    the 10 place holders
+    dt_reference_date = datetime.now()    
+    for i in range(0,10):
+      date_place_holder = f"%date_{i}%"
+      dt_i_date = dt_reference_date - timedelta(seconds=86400*(9-i))
+      news = news.replace(date_place_holder,
+                          dt_i_date.strftime("%a, %d %b %Y"))
+    # 2. Return the new string
+    return news
+    
   def do_GET(self):
     ''' This function implements the logic to server HTTP GET request
         of our web server.
@@ -23,27 +51,28 @@ class NewsFeedRequestHandler(BaseHTTPRequestHandler):
         Return:
           None - This function doesn't return anything
     '''
+    # 1. Setup response code and headers
     self.send_response(200)
-    self.send_header("Content-type", "text/html")
+    self.send_header("Content-type", "application/xml")
     self.end_headers()
-    self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-    self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-    self.wfile.write(bytes("<body>", "utf-8"))
-    self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-    self.wfile.write(bytes("</body></html>", "utf-8"))
+    # 2. Read the file containing the news from the file,
+    #    replace the place holders and put it into the response.
+    news = Path(self.news_file_path).read_text()    
+    self.wfile.write(bytes(self.update_dates(news), "utf-8"))
 
 class NewsFeedWebServer(Thread):
   ''' Class that will be used to run a web server in a thread concurrently 
       with the rest of the tasks.
   '''
 
-  def __init__(self):
+  def __init__(self, news_file_path):
     ''' Class initialization. No additional code needed at the moment.
         Parameters:
-          None - This function doesn't get any additional parameter
+          news_file_path - path to the file containing the XML file with the news
         Return:
           None - This function doesn't return anything
     '''
+    self.news_file_path = news_file_path
     Thread.__init__(self)
 
   def run(self):
@@ -55,6 +84,8 @@ class NewsFeedWebServer(Thread):
           None - This function doesn't return anything
     '''
     # 1. Starting the server (bound to TCP port 8080)
+    #    * News file path injected as described here: https://www.raspberrypi.org/forums/viewtopic.php?t=66940
+    NewsFeedRequestHandler.news_file_path = self.news_file_path
     web_server = HTTPServer(("localhost", 8080), NewsFeedRequestHandler)
     logging.info("Server started http://localhost:8080")
     # 2. Making the web server serve request forever until interrupted
@@ -81,7 +112,7 @@ def disk_filling(volume, size, duration):
       Return:
         None - This function doesn't return anything
   '''
-  logging.debug(f'Filling {volume} with {size}M in {duration} seconds.')
+  logging.info(f'Filling {volume} with {size}M in {duration} seconds.')
   # 1. Size of the files that will be created and will fill up the disk incrementally
   filesize = int(size/duration*1024*1024)
   logging.debug(f'Creating files {filesize}M big')
@@ -91,6 +122,121 @@ def disk_filling(volume, size, duration):
     with open(f'{volume}/fill_disk{i}.bin', 'wb') as fout:
          fout.write(os.urandom(filesize))
          fout.flush()
+def get_release_id(gh_repo_name, gh_personal_token):
+  ''' It get the release id of release v1.9
+      Parameters:
+        gh_repo           - repo name to be used in the URL to send all the POST requests
+        gh_personal_token - token needed to make the API calls
+      Return:
+        The release id of release v1.9 or -1 if there was any error.
+  '''
+  headers = {"Authorization": f"token {gh_personal_token}",
+             "Accept": "application/vnd.github.v3+json"}
+  url = f"https://api.github.com/repos/{gh_repo_name}/releases/tags/V1.9"
+  release_id = -1
+
+  # 1. Get id for release v1.9
+  response = requests.get(url, headers=headers)
+  logging.debug(f"response.text: {response.text}")
+  if response.status_code == requests.codes.ok:
+    release_id = response.json()['id']
+    logging.info(f"release_id: {release_id}")
+  else:
+    logging.error(f"Unable to get the id of release v1.9: {response.text}")
+  # 2. Return the value obtained
+  return release_id
+  
+def delete_gh_release(release_id, gh_repo_name, gh_personal_token):
+  ''' Remove release v1.9 from the Github
+      Parameters:
+        release_id        - the identifier needed to remove release
+        gh_repo_name      - repo name to be used in the URL to send all the POST requests
+        gh_personal_token - token needed to make the API calls
+      Return:
+        None - This function doesn't return anything
+  '''
+  headers = {"Authorization": f"token {gh_personal_token}",
+             "Accept": "application/vnd.github.v3+json"}
+  url =f"https://api.github.com/repos/{gh_repo_name}/releases/{release_id}"
+
+  # 1. Delete the release and log the result of the operation
+  #    Error code 204 (no_content) if all goes well
+  response = requests.delete(url, headers=headers)
+  logging.debug(f"response.text: {response.text}")
+  if response.status_code == requests.codes.no_content:
+    logging.info(f"release_id '{release_id}' removed!")
+  else:
+    logging.error(f"Error '{response.status_code}'. Unable to remove release v1.9 with id '{release_id}': {response.text}")
+
+def publish_gh_release(gh_repo_name, gh_personal_token):
+  ''' Publish release v1.9 one more time with the current date
+  '''
+  headers = {"Authorization": f"token {gh_personal_token}",
+             "Accept": "application/vnd.github.v3+json"}
+  url = f"https://api.github.com/repos/{gh_repo_name}/releases"
+  data = { "tag_name" : "V1.9", 
+           "name" : "V1.9", 
+           "body" : "Added new logging functionality, this will help the SRE debug issues faster. Logs are now spooled to disk much faster and can the verbosity level can be customised on the fly."}
+
+  # 1. Send the request to create the release one more time
+  #    Error code 201 (created) if all goes well
+  response = requests.post(url, data=json.dumps(data), headers=headers)
+  logging.debug(f"response.text: {response.text}")
+  if response.status_code == requests.codes.created:
+    logging.info(f"Release v1.9 created one more time")
+  else:
+    logging.error(f"Error '{response.status_code}'. Unable to create release v1.9: {response.text}")
+    
+def update_gh_release(gh_repo_name, gh_personal_token):
+  ''' Function making some API calls to the GH end point to refresh the release date
+      Parameters:
+        gh_repo_name      - repo name to be used in the URL to send all the POST requests
+        gh_personal_token - token needed to make the API calls
+      Return:
+        None - This function doesn't return anything
+  '''
+  # 1. Get the identifier of release v1.9
+  release_id = get_release_id(gh_repo_name, gh_personal_token)
+  if release_id > -1:
+    # 2. Remove release v1.9 if it was found
+    delete_gh_release(release_id, gh_repo_name, gh_personal_token)
+  # 3. Publish it one more time
+  publish_gh_release(gh_repo_name, gh_personal_token)
+
+def empty_data_volume(data_volume):
+  ''' Remove all the contents in the data volume where the random content is
+      created.
+      Parameters:
+        data_volume - the data volume that has to be empty
+      Return:
+        None - This function doesn't return anything
+  '''
+  files = glob.glob(f'{data_volume}/*', recursive=True)
+  logging.info(f"Removing all the contents in {data_volume}")
+
+  for f in files:
+    try:
+      os.remove(f)
+    except OSError as e:
+      logging.error(f"Error: {f} : {e.strerror}")
+                
+def reset(gh_repo_name, gh_personal_token, data_volume):
+  ''' Resets the environment to start the simulation from scratch by:
+        1. Updating the latest project release to have recent version matching
+           the rest of the simulation (disk filling, ....)
+        2. Removing the random data created in the data volume.
+      Parameters:
+        gh_repo_name      - repo name to be used in the URL to send all the POST requests
+        gh_personal_token - token needed to make the API calls
+        data_volume       - the data volume that has to be empty
+      Return:
+        None - This function doesn't return anything
+  '''
+  logging.info("Resetting simulation")
+  # 1. Update release v1.9 of the GH repo
+  update_gh_release(gh_repo_name, gh_personal_token)
+  # 2. Empty the data volume to have it ready for the disk filling
+  empty_data_volume(data_volume)
 
 def get_script_config(config_filename):
   ''' This function add all the configuration parameters of the script into a dictionary.
@@ -135,22 +281,29 @@ def add_event(event_scheduler, function_name, d_script_config):
       Return:
         None - This function doesn't return anything
   '''
+  simulation_duration = d_script_config['simulation_duration']
   # 1. Get the appropiate configuration parameters based on the function
   if function_name == "disk_filling":
     (volume, size, duration) = (d_script_config['data_volume'],
                                 d_script_config['volume_size'],
                                 d_script_config['filling_duration'])
-    event_scheduler.enter_recurring(duration, 0, disk_filling, (volume, size, duration))
+    event_scheduler.enter_recurring(simulation_duration, 0, disk_filling, (volume, size, duration))
+  elif function_name == "reset":
+    (repo_name, personal_token, volume) = (d_script_config['gh_repo_name'],
+                                           d_script_config['gh_personal_token'],
+                                           d_script_config['data_volume'])
+    # reset should happen before anything else (triggered at 90% of the simulation duration)
+    event_scheduler.enter_recurring(simulation_duration*0.9, 0, reset, (repo_name, personal_token, volume))
 
-def start_news_feed_service():
+def start_news_feed_service(news_file_path):
   ''' It starts a new thread running a web server which will be server news
       Parameters:
-        None - This function doesn't get any parameter
+        news_file_path - path to the file containing the XML file with the news
       Return:
         None - This function doesn't return anything
   '''
   # 1. Create a new instance of the web server and set it up as a daemon service
-  web_server = NewsFeedWebServer()
+  web_server = NewsFeedWebServer(news_file_path)
   web_server.daemon = True
   # 2. Start the web server
   web_server.start()  
@@ -172,15 +325,18 @@ if __name__ == "__main__":
   
   # 2. Starting the scheduler and adding tasks
   event_scheduler = start_event_scheduler()
+  add_event(event_scheduler, "reset", d_script_config)
   add_event(event_scheduler, "disk_filling", d_script_config)
-  #add_event(event_scheduler, "reset", d_script_config)
 
   # 3. Start the internal web server serving simulating the news feed service
-  start_news_feed_service()
+  start_news_feed_service(d_script_config['news_file_path'])
 
   # 4. Start the new release simulation
   # 4.1 Reset the context to start from the simulation from scratch
-  #reset()
+  (repo_name, personal_token, volume) = (d_script_config['gh_repo_name'],
+                                         d_script_config['gh_personal_token'],
+                                         d_script_config['data_volume'])
+  reset(repo_name, personal_token, volume)
   # 4.2 Start filling up the disk
   (volume, size, duration) = (d_script_config['data_volume'],
                               d_script_config['volume_size'],
